215efe29c44c157c74502516c5392cbb444ca5fc
HARI <harik87@gmail.com>
2016-12-17 00:58:13 +0530

    Login page design and added css, js files

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, flash, url_for, redirect, render_template, abort
 
 app = Flask(__name__)
-from app import views
+
+app.config.from_pyfile('app.cfg')
+
+web_root = "/web/"
+
+@app.route(web_root)
+def index():
+    return redirect(web_root+ "login")
+
+@app.route(web_root + "login", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        passwd = request.form['password']
+    return render_template('login.html', error=error)
