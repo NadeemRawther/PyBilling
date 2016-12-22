fe2a6b0056c678989790054e541353f1e9993ec7
HARI <harik87@gmail.com>
2016-12-23 00:40:03 +0530

    Adding some more modifications

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, flash, url_for, redirect, render_template, abort
 
 app = Flask(__name__)
 
 app.config.from_pyfile('app.cfg')
+db = SQLAlchemy(app)
+
+from app.models.user_model import Users
+db.create_all()
 
 web_root = "/web/"
 
@@ -18,4 +23,15 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         passwd = request.form['password']
+        if username == 'admin' and passwd == 'admin':
+            return render_template('index.html')
+        else:
+            user = Users.query.filter_by(username = username).first()
+            if not user or not user.verify_password(passwd):
+                return "Invalid username or password!"
+            return render_template('index.html')
     return render_template('login.html', error=error)
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    return render_template('new.html')
---
+++ app/models/user_model.py
@@ -0,0 +1,30 @@
+from app import *
+from sqlalchemy import Column, String, Integer
+
+# Define a User model
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column('id', db.Integer, primary_key=True)
+    role = db.Column(db.Integer)
+    username = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime)
+
+    def __init__(self, role, username, email, password):
+        self.role = role
+        self.username = username
+        self.password = password
+        self.email = email
+        self.created_at = datetime.utcnow()
+
+    def verify_password(self, password):
+        if(self.password == password):
+            return True
+        return False
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
